5db4e28a75a6697a794d5466d99b763b7b65f332
3-201412419 <dok2andtablo@naver.com>
2022-04-04 01:05:33 +0900

    Add files via upload

---
+++ Scripts/ObjectManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+
+    public string trap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
